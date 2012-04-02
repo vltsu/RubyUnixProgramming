e31a92520fc1d98bf8555efc0132c46f43e4b1c6
Vladimir Tsukanov <tsukanov.vl@gmail.com>
2012-04-03 00:26:28 +0400

    process termination

---
+++ 18_process_termination/exit_handlers.c
@@ -0,0 +1,32 @@
+#define _BSD_SOURCE
+#include <stdlib.h>
+#include "../lib/tlpi_hdr.h"
+#include "../lib/error_functions.c"
+
+static void atexitFunc1(void)
+{
+  printf("atexit function 1 called\n");
+}
+
+static void atexitFunc2(void)
+{
+  printf("atexit function 2 called \n");
+}
+
+static void onexitFunc(int exitStatus, void *arg)
+{
+  printf("on_exit function called: status=%d, arg=%ld\n", exitStatus, (long) arg);
+}
+
+int main(int argc, char *argv[])
+{
+  if(on_exit(onexitFunc, (void *) 10) != 0)
+    fatal("on_exit 1");
+  if(atexit(atexitFunc1) != 0)
+    fatal("atexit 1");
+  if(atexit(atexitFunc2) != 0)
+    fatal("atexit 2");
+  if(on_exit(onexitFunc, (void *) 20) != 0)
+    fatal("on_exit 2");
+  exit(2);
+}
